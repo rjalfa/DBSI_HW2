88ca5c94351f119e0226fd4b4161bbfc4665c6a0
Rounaq Jhunjhunu Wala <rounaqwl66@gmail.com>
2017-02-20 10:44:11 +0530

    Added Disk creator

---
+++ disk.cc
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if(argc < 3)
+	{
+		cerr << "Usage: ./disk <<path_to_disk_files>> <<number_of_blocks>>" << endl;
+		return 1;
+	}
+
+	string path(argv[1]);
+	unsigned int num_blocks = atoi(argv[2]); 
+	for(unsigned int i = 0; i < num_blocks; i ++)
+	{
+		ofstream out(path+to_string(i),ios::out);
+		if(!out.is_open()) {
+			cerr << "[ERROR]: Error creating block #" << i << endl;
+			return 2;
+		}
+		out.close();
+	}
+
+	return 0;
+}
